cd8708574c2bbc62a043e93e8e01a6d1eaca3a00
SARAVANAN99448 <saravanasaro0000@gmail.com>
2024-07-30 21:02:27 +0530

    Updated the file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar  from "./components/Navbar"
+import Search from "./components/Searchbox"
+import Products from "./components/Products"
+import About from "./components/About"
+import Footer from "./components/Footer"
+
+function App(){
+    return(<div>
+        <Navbar></Navbar><Search></Search><Products></Products><About></About><Footer></Footer>
+    </div>)
+}
+
+export default App
